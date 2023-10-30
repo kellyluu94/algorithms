b70318d21a138612c0fb3dc114f4e6f68b3876e8
kellyluu <kellyluu94@gmail.com>
2023-10-30 12:34:50 -0400

    randomized deque: implement array structure

--- week2/RandomizedQueue.java
+++ week2/RandomizedQueue.java
@@ -4,24 +4,27 @@
 // Iterator.
 // Each iterator must return the items in uniformly random order. The order of two or more iterators to the same randomized queue must be mutually independent; each iterator must maintain its own random order.
 
+// Corner cases.  Throw the specified exception for the following corner cases:
+// Throw an IllegalArgumentException if the client calls enqueue() with a null argument.
+// Throw a java.util.NoSuchElementException if the client calls either sample() or dequeue() when the randomized queue is empty.
+// Throw a java.util.NoSuchElementException if the client calls the next() method in the iterator when there are no more items to return.
+// Throw an UnsupportedOperationException if the client calls the remove() method in the iterator.
+
 // Unit testing.  Your main() method must call directly every public constructor and method to verify that they work as prescribed (e.g., by printing results to standard output).
 
 // Performance requirements.  Your randomized queue implementation must support each randomized queue operation (besides creating an iterator) in constant amortized time. That is, any intermixed sequence of m randomized queue operations (starting from an empty queue) must take at most cm steps in the worst case, for some constant c. A randomized queue containing n items must use at most 48n + 192 bytes of memory. Additionally, your iterator implementation must support operations next() and hasNext() in constant worst-case time; and construction in linear time; you may (and will need to) use a linear amount of extra memory per iterator.
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
-import org.w3c.dom.Node;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
-    private Node first;
-    private Node last;
+    private Item[] array;
     private int size;
 
     // construct an empty randomized queue
     public RandomizedQueue() {
-        first = null;
-        last = null;
+        array = (Itemp[]) new Object[10];
         size = 0;
     }
 
@@ -40,15 +43,10 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         if (item == null) {
             throw new IllegalArgumentException("Item must be non-null");
         }
-        Node newNode = new Node();
-        newNode.item = item;
-        newNode.prev = last;
-        if (last != null) {
-            last.zznext = newNode;
-        } else {
-            first = newNode;
+        if (size == array.length) {
+            resize(2 * array.length);
         }
-        last = newNode;
+        array[size] = item;
         size++;
     }
 
@@ -57,6 +55,16 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         if (isEmpty()) {
             throw new NoSuchElementException("Deque is empty");
         }
+        int index = StdRandom.uniformInt(size);
+        Item item = array[index];
+        for (int i = index; i < size - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        size--;
+        if (size > 0 && size > array.length / 4) {
+            resize(array.length / 2);
+        }
+        return item;
     }
 
     // return a random item (but do not remove it)
@@ -64,6 +72,17 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         if (isEmpty()) {
             throw new NoSuchElementException("Deque is empty");
         }
+        int index = StdRandom.uniformInt(size);
+        Item item = array[index];
+        return item;
+    }
+
+    private void resize(int capacity) {
+        Item[] newArray = (Item[]) new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = array[i];
+        }
+        array = newArray;
     }
 
     // return an independent iterator over items in random order
@@ -90,7 +109,6 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         public void remove() {
             throw new UnsupportedOperationException("Remove operation not supported");
         }
-
     }
 
     // unit testing (required)
